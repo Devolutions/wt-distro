98590291712d8fec0737b8befaaad3835ceaf06a
Marc-André Moreau <mamoreau@devolutions.net>
2022-12-08 11:00:23 -0500

    add vcrt-forwarders build flow

--- scripts/FixVCRTForwarders.ps1
+++ scripts/FixVCRTForwarders.ps1
@@ -2,13 +2,18 @@
 . $PSScriptRoot\common.ps1
 
 $NugetVersion = "1.0.4"
+
+if ($args.Count -gt 0) {
+	$NugetVersion = $args[0]
+}
+
 $NugetName = "Microsoft.VCRTForwarders.140"
 $NupkgBaseName = "Microsoft.VCRTForwarders.140.$NugetVersion"
 $NupkgFileName = "$NupkgBaseName.nupkg"
 
 Invoke-WebRequest "https://www.nuget.org/api/v2/package/$NugetName/$Version" -OutFile $NupkgFileName
 
-Remove-Item .\$NupkgBaseName -Recurse -Force
+Remove-Item .\$NupkgBaseName -Recurse -ErrorAction SilentlyContinue | Out-Null
 Expand-Archive -Path .\$NupkgFileName -Destination .\$NupkgBaseName
 Remove-Item .\$NupkgFileName -Force | Out-Null
 
@@ -19,19 +24,17 @@ $LegalCopyright = "Copyright 2020, $CompanyName"
 $VsProductVersion = $ProductVersion -Replace "(\d*).(\d*).(\d*).(\d*)", "`$1,`$2,`$3,`$4"
 
 # choco install reshack -y
-$Env:PATH += ";C:\Program Files (x86)\Resource Hacker"
+$ResHackCmd = Get-Item "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe"
 
-if (-Not (Get-Command -Name 'ResourceHacker.exe' -ErrorAction SilentlyContinue)) {
-	throw "ResourceHacker.exe not in PATH!"
+if (-Not (Get-Command -Name 'rc.exe' -ErrorAction SilentlyContinue)) {
+    throw "rc.exe not in PATH! Use a proper Visual Studio developer environment"
 }
 
-$RcCommand = Get-Item "C:\Program Files (x86)\Windows Kits\10\bin\*\x64\rc.exe" | sort -Descending | Select-Object -First 1
-
 Get-Item .\$NupkgBaseName\runtimes\win10-*\native\*\*.dll | ForEach-Object {
-	$FileName = $_.Name
-	$DllName = $_.FullName
+    $FileName = $_.Name
+    $DllName = $_.FullName
     $RcFile = $_.FullName -Replace '.dll', '.rc'
-	$ResFile = $_.FullName -Replace '.dll', '.res'
+    $ResFile = $_.FullName -Replace '.dll', '.res'
 
     Write-Host "$FileName / $RcFile"
 
@@ -51,21 +54,29 @@ Get-Item .\$NupkgBaseName\runtimes\win10-*\native\*\*.dll | ForEach-Object {
     }
 
     $VersionInfo = New-VsVersionInfo @Params
-	Set-Content -Path $RcFile -Value $VersionInfo -Force
+    Set-Content -Path $RcFile -Value $VersionInfo -Force
 
-	& $RcCommand.FullName '/nologo' $RcFile
+    & rc.exe '/nologo' $RcFile
 
-	Start-Process -FilePath "ResourceHacker.exe" -ArgumentList @(
-		'-open', $DllName,
-		'-save', $DllName,
-		'-resource', $ResFile,
-		'-action', 'addoverwrite',
-		'-mask', 'VersionInf',
-		'-log', 'CONSOLE'
-	) -Wait
+    Start-Process -FilePath $ResHackCmd.FullName -ArgumentList @(
+        '-open', $DllName,
+        '-save', $DllName,
+        '-resource', $ResFile,
+        '-action', 'addoverwrite',
+        '-mask', 'VersionInf',
+        '-log', 'CONSOLE'
+    ) -Wait
 
-	Remove-Item $RcFile -Force
-	Remove-Item $ResFile -Force
+    Remove-Item $RcFile -Force -ErrorAction SilentlyContinue
+    Remove-Item $ResFile -Force -ErrorAction SilentlyContinue
+}
+
+if ((Test-Path Env:CODESIGN_THUMBPRINT) -and (Test-Path Env:TIMESTAMP_SERVER)) {
+    $TimestampServer = $Env:TIMESTAMP_SERVER
+    $Certificate = Get-Item cert:\CurrentUser\My\$Env:CODESIGN_THUMBPRINT
+    Get-Item .\$NupkgBaseName\runtimes\win10-*\native\*\*.dll | ForEach-Object {
+        Set-AuthenticodeSignature -Certificate $Certificate -TimestampServer $TimestampServer $_
+    }
 }
 
 Remove-Item .\$NupkgFileName -Force -ErrorAction SilentlyContinue | Out-Null
